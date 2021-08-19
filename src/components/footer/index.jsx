2855932c38d677af088ee857077fe6a201866a6b
Umukoro Chinedu <umukorochinedu@gmail.com>
2021-08-19 15:18:27 -0700

    simple design with bootstrap

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,9 +3,9 @@ import { FaFacebookF, FaTwitter, FaLinkedinIn, FaWhatsapp, FaHeart } from 'react
 
 const Footer = () => {
   return (
-    <div className="py-5 footer-container text-light">
-      <div className="container py-5">
-        <div className="mx-auto  row">
+    <div className="py-5 mx-auto footer-container text-light">
+      <div className="container py-5 mx-auto">
+        <div className="mx-auto row">
           <div className="col-md-4">
             <h3 className="footer-title">About Me</h3>
             <p className="mt-3 footer-text">
@@ -13,8 +13,8 @@ const Footer = () => {
               growth. The more effort you put into improving your skills,
             </p>
           </div>
-          <div className="col-md-4">
-            <h3 className="footer-title">Quick Links</h3>
+          <div className="justify-center align-center col-md-4">
+            <h3 className="mx-auto footer-title">Quick Links</h3>
             <h5 className="mt-3 mb-0 quick-links">Home</h5>
             <h5 className="mt-1 mb-0 quick-links">About</h5>
             <h5 className="mt-1 quick-links">Contact</h5>
@@ -32,7 +32,7 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <p class=" quick-links">Copyright ©2021 All rights reserved | This template is made with <span className="text-danger"><FaHeart/></span>  by <strong className="text-info">Nerd35</strong></p>
+        <p class=" quick-links mt-3">Copyright ©2021 All rights reserved | This template is made with <span className="text-danger"><FaHeart/></span>  by <strong className="text-info">Nerd35</strong></p>
       </div>
     </div>
   );
