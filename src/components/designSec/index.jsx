a3524ff5fd7a516eecd24391b3fe7999a1b345fa
Umukoro Chinedu <umukorochinedu@gmail.com>
2021-08-19 19:04:46 -0700

    added dashboard

--- src/components/designSec/index.jsx
+++ src/components/designSec/index.jsx
@@ -2,14 +2,14 @@ import DesignImg from "../../assets/lernsection.png";
 
 const DesignSec = () => {
   return (
-    <div clasName="py-5 px-3 container text-center ">
+    <div clasName="py-5 container text-center design-div ">
       <h1 className="mt-5 text-center design-title">
         UNIQUE SCREENS THAT WORK PERFECTLY
       </h1>
-      <p className="design-text">
+      <p className="px-3 design-text">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
         tempor incididunt
-        <br /> ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+         ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
         nostrud exercitation.
       </p>
       <div className="text-center">
